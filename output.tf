5e9b0ea4360cb5cd6ff94b550d4ad96d48e9517f
vmokani <vishal.mokani@jeavio.com>
2024-12-09 17:50:44 +0530

    Commit output.tf file with comment email and identity O/P

--- output.tf
+++ output.tf
@@ -21,10 +21,10 @@ output "schedule_expression_cronValue" {
   value = aws_scheduler_schedule.lambda_terraform.schedule_expression
 }
 
-# output for email identity
-output "email_identity_vishal" {
-  value = data.aws_sesv2_email_identity.vishal.email_identity
-}
-output "email_verification_status_vishal" {
-  value = data.aws_sesv2_email_identity.vishal.verified_for_sending_status
-}
+# # output for email identity
+# output "email_identity_vishal" {
+#   value = data.aws_sesv2_email_identity.vishal.email_identity
+# }
+# output "email_verification_status_vishal" {
+#   value = data.aws_sesv2_email_identity.vishal.verified_for_sending_status
+# }
